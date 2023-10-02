356b8d7f53c94b7ddbae98ad199079617f4264c8
Derek Prieur <DPrieur123@gmail.com>
2023-10-02 16:25:15 -0400

    first

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hi</p>
+      <Button variant="destructive">Click Me</Button>
+    </div>
+  );
+}
