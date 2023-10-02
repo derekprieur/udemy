51183c9210512c4ffb23c98c64d52dfde5a5d587
Derek Prieur <DPrieur123@gmail.com>
2023-10-02 16:35:55 -0400

    update

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <p>Hi</p>
       <Button variant="destructive">Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
