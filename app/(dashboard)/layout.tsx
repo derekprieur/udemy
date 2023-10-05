95d32de7023b1c6667c77584af32e15cbc3262bb
Derek Prieur <DPrieur123@gmail.com>
2023-10-05 11:23:47 -0400

    updates

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from "./_components/Sidebar";
 import Navbar from "./_components/Navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  //
   return (
     <div className="h-full">
       <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
