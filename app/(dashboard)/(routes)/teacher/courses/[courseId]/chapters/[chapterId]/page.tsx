1165cad0163b3bee19f92e9708dae46156072d6d
Derek Prieur <DPrieur123@gmail.com>
2023-10-05 07:58:55 -0400

    updates

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import IconBadge from "@/components/IconBadge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import ChapterTitleForm from "./_components/ChapterTitleForm";
+import ChapterDescriptionForm from "./_components/ChapterDescriptionForm";
+import ChapterAccessForm from "./_components/ChapterAccessForm";
 
-const ChapterIdPage = () => {
-  return <div>ChapterIdPage</div>;
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
+
+  if (!chapter) {
+    return redirect("/");
+  }
+
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields}})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="w-full">
+          <Link
+            href={`/teacher/courses/${params.courseId}`}
+            className="mb-6 flex items-center text-sm transition hover:opacity-75"
+          >
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Back to course setup
+          </Link>
+          <div className="flex w-full items-center justify-between">
+            <div className="flex flex-col gap-y-2">
+              <h1 className="text-2xl font-medium">Chapter Creation</h1>
+              <span className="text-sm text-slate-700">
+                Complete all fields {completionText}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your chapter</h2>
+            </div>
+            <ChapterTitleForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">Access Settings</h2>
+            </div>
+            <ChapterAccessForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={Video} />
+            <h2 className="text-xl">Add a video</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterIdPage;
