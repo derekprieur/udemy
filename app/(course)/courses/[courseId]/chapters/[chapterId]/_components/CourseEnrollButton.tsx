ac489d4bfc50933d76a720bb29e014d6899edc7c
Derek Prieur <DPrieur123@gmail.com>
2023-10-06 17:01:12 -0400

    updates

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
   courseId: string;
@@ -8,7 +12,31 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ courseId, price }: CourseEnrollButtonProps) => {
-  return <button>Enroll for {formatPrice(price)}</button>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      className="w-full md:w-auto"
+      size="sm"
+      onClick={onClick}
+      disabled={isLoading}
+    >
+      Enroll for {formatPrice(price)}
+    </Button>
+  );
 };
 
 export default CourseEnrollButton;
